02594c302ffc474ed350ca9efd9ebf1bf53b7a7f
CON-ns <jiny5019@gmail.com>
2022-10-20 23:14:22 +0900

    all done

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 import * as THREE from "three";
-import fox from "./texture/fox.jpg";
-import dog from "./texture/dog.jpg";
-import bird from "./texture/bird.jpg";
-import redPanda from "./texture/red-panda.jpg";
-import cat from "./texture/cat.jpg";
-//imgを増やしたら、importで読み込む。
 
 let camera, scene, renderer,uniforms,geometry;
-
-//DOMのimg要素を配列で取得
-const imageArray = [...document.querySelectorAll('.slide-image')];
-
-//テクスチャの読み込み
-//(画像の表示順序は配列に入れる順番で制御、追加で読み込んだら定義して配列に入れる)
-const textures = [];
-const tex = new THREE.TextureLoader();
-const texture = tex.load(fox);
-const texture2 = tex.load(dog);
-const texture3 = tex.load(bird);
-const texture4 = tex.load(redPanda);
-const texture5 = tex.load(cat);
-textures.push(texture,texture2,texture3, texture4,texture5);
 
 //サイズを定義
 const canvas = document.getElementById("canvas");
@@ -32,7 +12,6 @@ const sizes = {
 function init() {
   //シーンを定義
   scene = new THREE.Scene();
-
   //カメラを定義(window座標とwebGL座標を一致させるため調整)
   const fov = 60,
     fovRad = (fov / 2) * (Math.PI / 180), //中心から左右30度ずつの視野角で丁度60度
@@ -93,9 +72,11 @@ const createMesh = (geometry,material) => {
 };
 
 //materialを生成する用の関数(uniformsの値を引数によって変更する目的)
-const createMaterial = (img,index) =>{
+const createMaterial = (img, index) => {
+
+  const texture = new THREE.TextureLoader().load(img.src);
   uniforms = {
-    uTexture: { value: textures[index] },
+    uTexture: { value: texture },
     uImageAspect: { value: img.naturalWidth / img.naturalHeight }, // 元画像のアスペクト比
     uPlaneAspect: { value: img.clientWidth / img.clientHeight }, // 画像描画サイズのアスペクト比
     uTime: { value: 0 },
@@ -110,7 +91,6 @@ const createMaterial = (img,index) =>{
   return material;
 }
 
-
 const imagePlaneArray = [];//テクスチャを適応したPlaneオブジェクトの配列
 //アニメーション実行用関数
 function animate() {
@@ -122,20 +102,18 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-//material,meshを格納する用の配列を定義
-const materials = [];
-const meshes = [];
+//DOMのimg要素を配列で取得
+const imageArray = [...document.querySelectorAll('.slide-image')];
+
 const main = () =>{
   window.addEventListener('load', () => {
-    //imgタグの数だけループを回して、順番に
+    //imgタグの数だけループを回して、順番にtextureを読み込みmeshを生成
     for (let i = 0; i < imageArray.length; i++) {
       geometry = new THREE.PlaneGeometry(1, 1, 100, 100);
       const img = imageArray[i];
       const material = createMaterial(img, i);
-      materials.push(material);
-      const mesh = createMesh(geometry, materials[i]);
-      meshes.push(mesh);
-      scene.add(meshes[i]);
+      const mesh = createMesh(geometry, material);
+      scene.add(mesh);
       const imagePlane = new ImagePlane(mesh, img);
       imagePlane.setParams();
       imagePlaneArray.push(imagePlane);
@@ -159,7 +137,6 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-  main();
   renderer.render(scene, camera);
 }
 
